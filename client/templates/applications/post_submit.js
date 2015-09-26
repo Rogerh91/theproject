b9e14afd458fd4530a6d931cfeea712ec83bec5e
Rogerh91 <roger.huang@thoughtbasin.com>
2015-09-26 13:20:09 -0400

    Made changes up to Chapter 7 of Discover Meteor

---
+++ client/templates/applications/post_submit.js
@@ -0,0 +1,26 @@
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    Meteor.call('postInsert', post, function(error, result) {
+      // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+      Router.go('postPage', {_id: result._id});  
+    });
+  }
+});
+
+//Line 1 is the standard helper for Meteor. It calls on postSubmit.html
+//Line 2 calls the function after the event happens. It defines a function and a variable--the variable the doesn't actually matter. We started it function(a)
+//Line 3 prevents the default behavior of a form submission and breaks it to the new logic
+//Line 5 defines a local variable post
+//Line 6 defines a new url variable through finding the value assigned to the label name "url"
+//Line 7 does the same with the label name "title"
+//Line 10 places the new value into the posts database and take out the id of post
+//Line 11 goes to the defined page. 
