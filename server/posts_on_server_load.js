6464351ffaddfeab1e4d435ad1759ce54dcc9a04
Rogerh91 <roger.huang@thoughtbasin.com>
2015-10-23 08:38:01 -0400

    Implemented pagination on posts subscription

--- server/posts_on_server_load.js
+++ server/posts_on_server_load.js
@@ -1,23 +1,24 @@
 if (Posts.find().count() === 0) {
 
   Posts.insert({
-    title: "Facebook",
-    url: "http://www.facebook.com"
+    title: 'The Meteor Book',
+    userId: 1,
+    author: "tom",
+    url: 'http://themeteorbook.com',
+    submitted: "10/23/2015",
+    commentsCount: 0
   });
 
-  Posts.insert({
-    title: "Meteor",
-    url: "http://www.meteor.com"
-  });
-
-  Posts.insert({
-    title: "Twitter",
-    url: "http://www.twitter.com"
-  });
-
-  Posts.insert({
-    title: "Shopify",
-    url: "http://www.shopify.com"
-  });
-
+  for (var i = 0; i < 15; i++) {
+    Posts.insert({
+      title: 'Test post #' + i,
+      author: "sacha",
+      userId: 2,
+      url: 'http://google.com/?q=test-' + i,
+      submitted: "10/23/2015",
+      commentsCount: 0
+    });
+  }
 }
+
+//these fixtures are broken, need to find a way to debug date(now)
