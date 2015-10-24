ccefbe49941ab9fa15e3207226bdca5adcb54ceb
Rogerh91 <roger.huang@thoughtbasin.com>
2015-10-24 17:01:24 -0400

    Finished upvoting

--- client/templates/posts/posts_item.js
+++ client/templates/posts/posts_item.js
@@ -8,6 +8,15 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  },
+
+  upvotedClass: function(){
+    var userId = Meteor.userId();
+    if (userId && !_.include(this.upvoters, userId)) {
+      return 'btn-primary upvotable';
+       } else {
+      return 'disabled';
+    }
   }
 });
 
@@ -18,8 +27,15 @@ Template.postItem.events({
     var confirm = window.confirm("Are you sure you want to delete this discussion?");
     if (confirm){
       Meteor.call('postDelete', documentId); 
-      Router.go('/');  
+      Router.go('home');  
       alert("You have deleted the previous post.");
     }
   }
-})
+}); 
+
+Template.postItem.events({
+  'click .upvotable': function(e) {
+    e.preventDefault();
+    Meteor.call('upvote', this._id);
+  }
+});
